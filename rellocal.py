d2458b71187d9c7361ac5205c137f6205a283654
JulianSVC <126276991+JulianSVC@users.noreply.github.com>
2025-05-27 20:32:11 -0500

    Update rellocal.py

--- rellocal.py
+++ rellocal.py
@@ -1,4 +1,4 @@
-n1 = int(input("ingrese el primer numero:  "))
+n1 = int(input("ingrese el primer numero:  "))
 n2 = int(input("ingrese el segundo numero: "))
 sum = n1 + n2
 print("la suma es:", sum)
